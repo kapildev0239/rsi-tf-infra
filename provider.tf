b4d7ddac12c7125d3b7bda3a21317ba9c08e1b6b
kapildev0239 <kapil.dev@rsystems.com>
2026-03-17 19:47:16 +0530

    Update provider.tf with new subscription_id

--- provider.tf
+++ provider.tf
@@ -1,12 +1,6 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0"
-    }
-  }
-}
+# Updated provider.tf
 
 provider "azurerm" {
-  features {}  
+  features {}
+  subscription_id = "70224b4b-9197-49f2-a41e-2c0fe4f22ff0"
 }
